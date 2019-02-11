efff6334121e893b25dc9ee4308d53e3a4a20901
navya budati <navya@gmail.com>
2019-02-11 09:25:13 +0000

    python prgm

---
+++ sum.py
@@ -0,0 +1,5 @@
+FirstNumber = int(input('To perform addition of two numbers, enter the first number:'))
+SecondNumber = int(input('Enter the second number:'))
+Sum = FirstNumber + SecondNumber
+print 'The sum of %d and %d is %d.' %(FirstNumber, SecondNumber, Sum)
+  
